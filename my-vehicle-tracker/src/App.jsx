e43b0facb2910b00908585091a0431f55598f03f
rushikesh ughade <you@example.com>
2024-07-26 11:46:14 +0530

    first commit

---
+++ my-vehicle-tracker/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Map from './Map'; // Import the Map component
+
+function App() {
+  return (
+    <div className="App">
+      <Map />
+    </div>
+  );
+}
+
+export default App;
